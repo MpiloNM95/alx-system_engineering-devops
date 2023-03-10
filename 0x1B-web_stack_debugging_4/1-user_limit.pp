7b127921531517348de9373f799f60565f748461
MpiloNM95 <mpilonmafu95@gmail.com>
2023-03-10 00:07:14 -0800

    done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,11 @@
 # Enable the user holberton to login and open files without error
 
-# Increase hard file limit for Holberton user.
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'fixithard':
+  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 55000/g" /etc/security/limits.conf',
+  provider => 'shell'
 }
 
-# Increases soft file limit for Holberton user.
-exec { 'increases-soft-file-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/50000" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'fixitsoft':
+  command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 55000/g" /etc/security/limits.conf',
+  provider => 'shell'
 }
